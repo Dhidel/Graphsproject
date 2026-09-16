e83f6f2d884adea32625c5f72f276cbe80b0a0ce
DhidelOsorio <djosorio@ufm.edu>
2026-09-16 16:26:42 -0600

    Se hizo el filtro de columnas que se utilizarán, y se definió en el readme los aspectos principales del grafo

--- argentina.py
+++ argentina.py
@@ -3,15 +3,41 @@ Datos y Grafo de los pases de Argentina durante la fase de grupos del Qatar 2022
 """
 
 import pandas as pd
-
 lec = pd.read_csv("pases_argentina.csv")
 
-FG = lec[lec["fase"] == "Group Stage"]
-# print (FG)
+# - 3.1 FILTROS
 
-"""
-Los datos que voy a utilizar son: match
-"""
+columnas = ["match_id", "fase", "oponente", "minuto", "jugador_nombre", "receptor_nombre", "resultado", "longitud_pase"]
+lf= pd.read_csv("pases_argentina.csv", usecols= columnas)
+fg = lf[lf["fase"] == "Group Stage"]
+# print(lf)
+
+
+
+
+
+
+
+
+
+
+
+
+# # - Partidos
+
+# P1 = lec[lec["match_id"] == 3857300]
+# # print(P1)
+
+# P2 = lec[lec["match_id"] == 3857289]
+# # print(P2)
+
+# P3 = lec[lec["match_id"] == 3857264]
+# print(P3)
+
+
+
+
+
 
 
 
